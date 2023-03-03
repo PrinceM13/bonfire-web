b661b1710f3db9edc3d86fef5fa8f238ce1d8356
nachweerasiri <nach.weerasiri@gmail.com>
2023-03-04 01:32:54 +0700

    Active nav and filter

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,29 +1,42 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import ChatIcon from "../assets/icons/ChatIcon";
+import ChatIconCurrent from "../assets/icons/ChatIconCurrent";
 import CreateEventIcon from "../assets/icons/CreateEventIcon";
+import CreateEventIconCurrent from "../assets/icons/CreateEventIconCurrent";
 import HomeIcon from "../assets/icons/HomeIcon";
+import HomeIconCurrent from "../assets/icons/HomeIconCurrent";
 import NotificationIcon from "../assets/icons/NotificationIcon";
+import NotificationIconCurrent from "../assets/icons/NotificationIconCurrent";
 import PromotionIcon from "../assets/icons/PromotionIcon";
+import PromotionIconCurrent from "../assets/icons/PromotionIconCurrent";
 
 export default function Footer({ content = "" }) {
+  const location = useLocation();
+
   return (
     <div className="flex justify-between items-center bg-white h-[8vh] px-5 bottom-[-1px] right-0 fixed w-full shadow-lg">
       {content === "" && (
         <>
-          <Link to="/">
-            <HomeIcon />
-          </Link>
+          <Link to="/">{location.pathname === "/" ? <HomeIconCurrent /> : <HomeIcon />}</Link>
           <Link to="/promotions">
-            <PromotionIcon />
+            {location.pathname === "/promotions" ? <PromotionIconCurrent /> : <PromotionIcon />}
           </Link>
           <Link to="/create-event">
-            <CreateEventIcon />
+            {location.pathname === "/create-event" ? (
+              <CreateEventIconCurrent />
+            ) : (
+              <CreateEventIcon />
+            )}
           </Link>
           <Link to="/notifications">
-            <NotificationIcon />
+            {location.pathname === "/notifications" ? (
+              <NotificationIconCurrent />
+            ) : (
+              <NotificationIcon />
+            )}
           </Link>
           <Link to="/chatroom">
-            <ChatIcon />
+            {location.pathname === "/chatroom" ? <ChatIconCurrent /> : <ChatIcon />}
           </Link>
         </>
       )}
