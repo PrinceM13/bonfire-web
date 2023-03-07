bc3bb9a46664fc59f652bc3772e299f14793adf4
aeynd <aaeynd@gmail.com>
2023-03-07 20:43:57 +0700

    combine map

--- src/features/map/Map.js
+++ src/features/map/Map.js
@@ -4,22 +4,22 @@ import {
   useLoadScript,
   Marker,
   InfoWindow,
-  DirectionsRenderer
+  DirectionsRenderer,
+  Circle
 } from "@react-google-maps/api";
 import CurrerntLocation from "./CurrentLocation";
 import SearchLocation from "./SearchLocation";
 import MapMarkers from "./MapMarkers";
-// import MapMultiMarkers from "./MapMultiMarker";
 import MapDirection from "./MapDirection";
 import mapStyles from "./mapStyles";
 import PinGoogleMapSmall from "../../assets/icons/PinGoogleMapSmall";
 import CurrentPoint from "../../assets/icons/CurrentPoint";
 // import ChooseDestination from "../../assets/icons/ChooseDestination";
 
-const libraries = ["places"];
+const libraries = ["places","geometry"];
 const mapContainerStyle = {
-  height: "92vh",
-  width: "100vw"
+  height: "90vh",
+  width: "screen"
 };
 
 const options = {
@@ -33,14 +33,13 @@ const center = {
   lng: 100.5235765
 };
 
-export default function Map({ isMultiMarker = true }) {
+export default function Map({ isMultiMarker = true, handleChange }) {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries
   });
 
-  const [markers, setMarkers] = useState([]);
-  // const [markers, setMarkers] = useState({});
+  const [markers, setMarkers] = useState([{ lat: 0, lng: 0, location: "" }]);
   const [selected, setSelected] = useState(null);
   const [locationInfo, setLocationInfo] = useState({ name: "", detail: "" });
 
@@ -50,42 +49,104 @@ export default function Map({ isMultiMarker = true }) {
 
   const [circle, setCircle] = useState(null);
 
-  useEffect(() => {
-    const storedMarkers = JSON.parse(localStorage.getItem("markers"));
-
-    if (storedMarkers) {
-      setMarkers(storedMarkers);
-    }
-    console.log(storedMarkers);
-  }, []);
 
   useEffect(() => {
     localStorage.setItem("markers", JSON.stringify(markers));
   }, [markers]);
 
+  const handleSetMarker = newMarkers => {
+    console.log(newMarkers);
+    if (!isMultiMarker) {
+      setMarkers(newMarkers);
+      const { lat, lng } = newMarkers[0];
+      const geocoder = new window.google.maps.Geocoder();
+      geocoder.geocode({ location: { lat, lng } }, (results, status) => {
+        if (status === "OK") {
+          const placeName = results[0].formatted_address;
+          handleChange({
+            latitude: lat || "",
+            longitude: lng || "",
+            location: placeName || ""
+          });
+        }
+      });
+    } else {
+      setMarkers(current => [...current, ...newMarkers]);
+    }
+  };
+  // const handleSetMarker = newMarkers => {
+  //   console.log("-----------2-2-2", isMultiMarker);
+  //   console.log("newMarkerrrrrrrrrr", newMarkers);
+
+  //   if (!isMultiMarker) {
+  //     setMarkers(newMarkers);
+
+  //     handleChange({
+  //       latitude: newMarkers[0]?.lat || "",
+  //       longitude: newMarkers[0]?.lng || "",
+  //       location: ""
+  //       // location: "sdvsdvsdv"
+  //     });
+  //   } else {
+  //     console.log("inside ??????", markers);
+  //     console.log("why new wrong ?????", newMarkers);
+  //     if (markers[0].lat === 0 && markers[0].lng === 0) {
+  //       setMarkers(newMarkers);
+  //     } else {
+  //       setMarkers(current => [...current, ...newMarkers]);
+  //     }
+  //   }
+  // };
+
   const onMapClick = useCallback(
     e => {
-      if (!isMultiMarker) {
-        setMarkers([
-          {
-            lat: e.latLng.lat(),
-            lng: e.latLng.lng(),
-            time: new Date()
-          }
-        ]);
-      } else {
-        setMarkers(current => [
-          ...current,
-          {
-            lat: e.latLng.lat(),
-            lng: e.latLng.lng(),
-            time: new Date()
-          }
-        ]);
-      }
+      console.log("fist click pls");
+      handleSetMarker([{ lat: e.latLng.lat(), lng: e.latLng.lng() }]);
+      // if (!isMultiMarker) {
+      //   setMarkers([
+      //     {
+      //       lat: e.latLng.lat(),
+      //       lng: e.latLng.lng(),
+      //       time: new Date()
+      //     }
+      //   ]);
+      // } else {
+      //   setMarkers(current => [
+      //     ...current,
+      //     {
+      //       lat: e.latLng.lat(),
+      //       lng: e.latLng.lng(),
+      //       time: new Date()
+      //     }
+      //   ]);
+      // }
     },
     [markers]
   );
+  // const onMapClick = useCallback(
+  //   e => {
+  //     console.log("fist click pls");
+  //     if (!isMultiMarker) {
+  //       setMarkers([
+  //         {
+  //           lat: e.latLng.lat(),
+  //           lng: e.latLng.lng(),
+  //           time: new Date()
+  //         }
+  //       ]);
+  //     } else {
+  //       setMarkers(current => [
+  //         ...current,
+  //         {
+  //           lat: e.latLng.lat(),
+  //           lng: e.latLng.lng(),
+  //           time: new Date()
+  //         }
+  //       ]);
+  //     }
+  //   },
+  //   [markers]
+  // );
 
   // const onMapClick = useCallback(e => {
   //   setMarkers([{
@@ -107,13 +168,13 @@ export default function Map({ isMultiMarker = true }) {
   // }, []);
 
   const mapRef = useRef();
-  const onMapLoad = useCallback((map) => {
+  const onMapLoad = useCallback(map => {
     mapRef.current = map;
   }, []);
 
   const panTo = useCallback(({ lat, lng }) => {
     mapRef.current.panTo({ lat, lng });
-    mapRef.current.setZoom(19);
+    mapRef.current.setZoom(14);
   }, []);
 
   if (loadError) return "Error";
@@ -121,7 +182,7 @@ export default function Map({ isMultiMarker = true }) {
 
   return (
     <>
-      <div className="px-8 pt-[5vh] bg-white h-[13vh] pb-[2.7vh] top-0 left-0 fixed w-full shadow-lg flex z-10">
+      <div className=" bg-white px-0 pt-[5vh] h-[13vh] pb-[2.7vh] top-0 left-0 relative w-full shadow-lg flex z-10">
         <div className="bg-gradient-to-b from-[#6A6A6A] to-[#D4D4D4] p-[1.5px] w-full rounded-full">
           <div className="flex w-full items-center justify-center bg-white rounded-full px-3 py-1 shadow-md gap-2">
             <div>
@@ -132,6 +193,7 @@ export default function Map({ isMultiMarker = true }) {
               setMarkers={setMarkers}
               setSelected={setSelected}
               setLocationInfo={setLocationInfo}
+              handleChange={handleChange}
             />
           </div>
         </div>
@@ -162,6 +224,7 @@ export default function Map({ isMultiMarker = true }) {
             setLocationInfo={setLocationInfo}
             circle={circle}
             setCircle={setCircle}
+            handleChange={handleChange}
           />
           {/* {directions && (
             <DirectionsRenderer
@@ -176,7 +239,14 @@ export default function Map({ isMultiMarker = true }) {
             />
           )} */}
           <div className="fixed left-2 bottom-24">
-            <CurrerntLocation panTo={panTo} />
+            <CurrerntLocation
+              panTo={panTo}
+              mapRef={mapRef}
+              circle={circle}
+              setCircle={setCircle}
+              setMarkers={setMarkers}
+              handleChange={handleChange}
+            />
           </div>
           {/* <div className="fixed left-2 bottom-24">
             <ChooseDestination />
--- src/features/map/MapMarkers.js
+++ src/features/map/MapMarkers.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { InfoWindow, Marker } from "@react-google-maps/api";
 
 export default function MapMarkers({
@@ -9,69 +9,55 @@ export default function MapMarkers({
   locationInfo,
   setLocationInfo,
   circle,
-  setCircle
+  setCircle,
+  handleChange,
+  radius
 }) {
   const geocoder = new window.google.maps.Geocoder();
 
+  // const filteredMarkers = markers.filter(marker => {
+  //   if (!circle) {
+  //     return true;
+  //   }
+  //   const distance = window.google.maps.geometry.spherical.computeDistanceBetween(
+  //     new window.google.maps.LatLng(circle.center),
+  //     new window.google.maps.LatLng(marker)
+  //   );
+  //   return distance <= radius;
+  // });
+
   return (
     <div>
-      {/* {Object.keys(markers).length !== 0 && (
+      {/* {filteredMarkers?.map((marker, index) => ( */}
+    {markers?.map(marker => (
         <Marker
           // key={`${marker.lat}-${marker.lng}`}
-          position={{ lat: markers.lat, lng: markers.lng }}
-          zIndex={1}
-          draggable={true}
-          onDragEnd={event => {
-            markers = {
-              lat: event.latLng.lat(),
-              lng: event.latLng.lng()
-            };
-            setMarkers(markers);
-          }}
-          onClick={() => {
-            geocoder.geocode(
-              { location: { lat: markers.lat, lng: markers.lng } },
-              (results, status) => {
-                console.log(results);
-                if (status === "OK") {
-                  setLocationInfo({
-                    name: `${results[0].formatted_address}`,
-
-                    detail: `Lat: ${markers.lat}, Lng: ${markers.lng}`,
-                    googleMapsLink: `https://www.google.com/maps/search/?api=1&query=${markers.lat},${markers.lng}`
-                  });
-                }
-              }
-            );
-            setSelected(markers);
-          }}
-        />
-      )} */}
-
-      {markers?.map(marker => (
-        <Marker
-          // key={`${marker.lat}-${marker.lng}`}
+          // key={index}
           position={{ lat: marker.lat, lng: marker.lng }}
-          zIndex={1}
-          draggable={true}
-          onDragEnd={event => {
-            const index = markers.findIndex(m => m.lat === marker.lat && m.lng === marker.lng);
-            const newMarkers = [...markers];
-            newMarkers[index] = {
-              lat: event.latLng.lat(),
-              lng: event.latLng.lng()
-            };
-            setMarkers(newMarkers);
-          }}
+          // visible={marker.visible}
+          // draggable={true}
+          // animation={window.google.maps.Animation.BOUNCE}
+          // onDragEnd={event => {
+          //   const index = markers.findIndex(m => m.lat === marker.lat && m.lng === marker.lng);
+          //   console.log("index", index);
+          //   const newMarkers = [...markers];
+          //   newMarkers[index] = {
+          //     lat: event.latLng.lat(),
+          //     lng: event.latLng.lng()
+          //   };
+          //   console.log("wtfffffffffffff", newMarkers[index]);
+          //   setMarkers(newMarkers);
+
+          // }}
           onClick={() => {
             geocoder.geocode(
               { location: { lat: marker.lat, lng: marker.lng } },
               (results, status) => {
                 console.log(results);
+                console.log("in hereeeeeeeeeee");
                 if (status === "OK") {
                   setLocationInfo({
                     name: `${results[0].formatted_address}`,
-
                     detail: `Lat: ${marker.lat}, Lng: ${marker.lng}`,
                     googleMapsLink: `https://www.google.com/maps/search/?api=1&query=${marker.lat},${marker.lng}`
                   });
@@ -83,7 +69,56 @@ export default function MapMarkers({
         />
       ))}
 
-      {/* {selected ? (
+      {selected ? (
+        <InfoWindow
+          position={{ lat: selected.lat, lng: selected.lng }}
+          onCloseClick={() => setSelected(null)}
+        >
+          <div>
+            <h2>{locationInfo.name}</h2>
+            <p className="font-bold">{locationInfo.detail}</p>
+            {locationInfo.googleMapsLink && (
+              <a
+                href={locationInfo.googleMapsLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-700 hover:underline"
+              >
+                View on Google Maps
+              </a>
+            )}
+            <button
+              className="text-red-500 hover:underline font-bold"
+              onClick={() => {
+                console.log(typeof handleChange);
+                console.log("This is handle change!!!!!!!!!", handleChange);
+                console.log(setSelected);
+                handleChange({ latitude: "", longitude: "", location: "" });
+                setMarkers(
+                  markers.filter(
+                    marker => marker.lat !== selected.lat || marker.lng !== selected.lng
+                  )
+                );
+                setSelected(null);
+                setLocationInfo({ name: "", detail: "", googleMapsLink: "" });
+
+                if (circle) {
+                  circle.setMap(null);
+                  setCircle(null);
+                }
+              }}
+            >
+              Remove pin
+            </button>
+          </div>
+        </InfoWindow>
+      ) : null}
+    </div>
+  );
+}
+
+{
+  /* {selected ? (
         <InfoWindow
           position={{ lat: selected.lat, lng: selected.lng }}
           onCloseClick={() => setSelected(null)}
@@ -117,43 +152,5 @@ export default function MapMarkers({
             </button>
           </div>
         </InfoWindow>
-      ) : null} */}
-
-      {selected ? (
-        <InfoWindow
-          position={{ lat: selected.lat, lng: selected.lng }}
-          onCloseClick={() => setSelected(null)}
-        >
-          <div>
-            <h2>{locationInfo.name}</h2>
-            <p className="font-bold">{locationInfo.detail}</p>
-            {locationInfo.googleMapsLink && (
-              <a
-                href={locationInfo.googleMapsLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-700 hover:underline"
-              >
-                View on Google Maps
-              </a>
-            )}
-            <button
-              className="text-red-500 hover:underline font-bold"
-              onClick={() => {
-                setMarkers(markers.filter(marker => marker.time !== selected.time));
-                setSelected(null);
-                setLocationInfo({ name: "", detail: "", googleMapsLink: "" });
-                if (circle) {
-                  circle.setMap(null);
-                  setCircle(null);
-                }
-              }}
-            >
-              Remove pin
-            </button>
-          </div>
-        </InfoWindow>
-      ) : null}
-    </div>
-  );
+      ) : null} */
 }
