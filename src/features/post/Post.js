c232ad9a9c7ff8142e554b9404c381cb73390eb2
mewpotchanad <natthapotchanad@gmail.com>
2023-03-08 19:01:24 +0700

    edit post

--- src/features/post/Post.js
+++ src/features/post/Post.js
@@ -1,8 +1,8 @@
 export default function Post({ children, createEvent = "" }) {
   return (
     <div className={createEvent}>
-      <div className=" p-2 w-full bg-white rounded-lg shadow-lg content-center ">
-        <div className="p-4 rounded-lg">{children}</div>
+      <div className=" p-2 w-full rounded-lg content-center bg-[#ffffffbb] drop-shadow-lg">
+        <div className="p-0 rounded-lg">{children}</div>
       </div>
     </div>
   );
